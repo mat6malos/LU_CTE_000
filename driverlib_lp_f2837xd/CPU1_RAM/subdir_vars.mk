304f7351632f900d85e33b56828246f127be655a
Lucas Matamoros <lucas.matamoros@usm.cl>
2024-12-18 11:33:46 -0300

    First commit

---
+++ driverlib_lp_f2837xd/CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,81 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../2837xD_RAM_lnk_cpu1.cmd \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/headers/cmd/F2837xD_Headers_nonBIOS_cpu1.cmd 
+
+LIB_SRCS += \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ccs/Debug/driverlib.lib 
+
+ASM_SRCS += \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_CodeStartBranch.asm \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_usDelay.asm 
+
+C_SRCS += \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_DefaultISR.c \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_Gpio.c \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_PieCtrl.c \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_PieVect.c \
+/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_SysCtrl.c \
+../lab_main.c 
+
+C_DEPS += \
+./F2837xD_DefaultISR.d \
+./F2837xD_Gpio.d \
+./F2837xD_PieCtrl.d \
+./F2837xD_PieVect.d \
+./F2837xD_SysCtrl.d \
+./lab_main.d 
+
+OBJS += \
+./F2837xD_CodeStartBranch.obj \
+./F2837xD_DefaultISR.obj \
+./F2837xD_Gpio.obj \
+./F2837xD_PieCtrl.obj \
+./F2837xD_PieVect.obj \
+./F2837xD_SysCtrl.obj \
+./F2837xD_usDelay.obj \
+./lab_main.obj 
+
+ASM_DEPS += \
+./F2837xD_CodeStartBranch.d \
+./F2837xD_usDelay.d 
+
+OBJS__QUOTED += \
+"F2837xD_CodeStartBranch.obj" \
+"F2837xD_DefaultISR.obj" \
+"F2837xD_Gpio.obj" \
+"F2837xD_PieCtrl.obj" \
+"F2837xD_PieVect.obj" \
+"F2837xD_SysCtrl.obj" \
+"F2837xD_usDelay.obj" \
+"lab_main.obj" 
+
+C_DEPS__QUOTED += \
+"F2837xD_DefaultISR.d" \
+"F2837xD_Gpio.d" \
+"F2837xD_PieCtrl.d" \
+"F2837xD_PieVect.d" \
+"F2837xD_SysCtrl.d" \
+"lab_main.d" 
+
+ASM_DEPS__QUOTED += \
+"F2837xD_CodeStartBranch.d" \
+"F2837xD_usDelay.d" 
+
+ASM_SRCS__QUOTED += \
+"/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_CodeStartBranch.asm" \
+"/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_usDelay.asm" 
+
+C_SRCS__QUOTED += \
+"/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_DefaultISR.c" \
+"/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_Gpio.c" \
+"/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_PieCtrl.c" \
+"/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_PieVect.c" \
+"/Applications/ti/c2000/C2000Ware_5_04_00_00/device_support/f2837xd/common/source/F2837xD_SysCtrl.c" \
+"../lab_main.c" 
+
+
