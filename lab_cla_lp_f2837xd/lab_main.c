cf0f4ca9167e5758a6e787c75797b76a37839785
Lucas Matamoros <lucas.matamoros@usm.cl>
2024-12-27 12:51:39 -0300

    agregue el lab de cla y ajuste hasta el segundo pwm, viene el adc

---
+++ lab_cla_lp_f2837xd/lab_main.c
@@ -0,0 +1,27 @@
+//#############################################################################
+//
+// FILE:   lab_main.c
+//
+// TITLE:  C2000 Academy Lab Startup Project
+//
+//
+// This example is a startup project for C2000 Academy lab.
+//
+//#############################################################################
+//
+// Included Files
+//
+#include "driverlib.h"
+#include "device.h"
+
+//
+// Main
+//
+void main(void)
+{
+
+}
+
+//
+// End of File
+//
