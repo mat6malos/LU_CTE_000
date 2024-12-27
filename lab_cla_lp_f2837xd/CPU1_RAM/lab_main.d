bc9d534d771f8250727a48b34b5a551eaf85cf59
Lucas Matamoros <lucas.matamoros@usm.cl>
2024-12-27 15:24:13 -0300

    finalicé syscfg, y agregue todo codigo a main.

---
+++ lab_cla_lp_f2837xd/CPU1_RAM/lab_main.d
@@ -0,0 +1,276 @@
+# FIXED
+
+lab_main.obj: ../lab_main.c
+lab_main.obj: /Users/lucasmatamoros/Desktop/Practica\ CTE/Workspace/lab_cla_lp_f2837xd/device/driverlib.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/adc.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/stdbool.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/_ti_config.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/linkage.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/stdint.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/_stdint40.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/stdint.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/cdefs.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/_types.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/machine/_types.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/machine/_stdint.h
+lab_main.obj: /Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/_stdint.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cpu.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/debug.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/asysctl.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/can.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sysctl.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_otp.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/interrupt.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cla.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/clb.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cmpss.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cputimer.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dac.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dcsm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dma.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ecap.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/emif.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/epwm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/eqep.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/flash.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/gpio.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/xbar.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/i2c.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ipc.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/mcbsp.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/memcfg.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sci.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sdfm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/spi.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/upp.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/version.h
+lab_main.obj: /Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h
+lab_main.obj: syscfg/board.h
+lab_main.obj: /Users/lucasmatamoros/Desktop/Practica\ CTE/Workspace/lab_cla_lp_f2837xd/device/device.h
+lab_main.obj: ../lab_shared.h
+lab_main.obj: /Users/lucasmatamoros/Desktop/Practica\ CTE/Workspace/lab_cla_lp_f2837xd/device/device.h
+
+../lab_main.c:
+
+/Users/lucasmatamoros/Desktop/Practica\ CTE/Workspace/lab_cla_lp_f2837xd/device/driverlib.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/adc.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/stdbool.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/_ti_config.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/linkage.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/stdint.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/_stdint40.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/stdint.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/cdefs.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/_types.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/machine/_types.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/machine/_stdint.h:
+
+/Applications/ti/ccs2000/ccs/tools/compiler/ti-cgt-c2000_22.6.1A23259/include/sys/_stdint.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cpu.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/debug.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/asysctl.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/can.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sysctl.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_otp.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/interrupt.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cla.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/clb.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cmpss.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/cputimer.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dac.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dcsm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/dma.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ecap.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/emif.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/epwm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/eqep.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/flash.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/gpio.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/xbar.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/i2c.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ipc.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/mcbsp.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/memcfg.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sci.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/sdfm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/spi.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/upp.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/version.h:
+
+/Applications/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h:
+
+syscfg/board.h:
+
+/Users/lucasmatamoros/Desktop/Practica\ CTE/Workspace/lab_cla_lp_f2837xd/device/device.h:
+
+../lab_shared.h:
+
+/Users/lucasmatamoros/Desktop/Practica\ CTE/Workspace/lab_cla_lp_f2837xd/device/device.h:
+
--- lab_cla_lp_f2837xd/lab_main.c
+++ lab_cla_lp_f2837xd/lab_main.c
@@ -12,16 +12,99 @@
 // Included Files
 //
 #include "driverlib.h"
+#include "board.h"
+#include "lab_shared.h"
 #include "device.h"
+#define BUF_BITS    7                           // Buffer bits <= 16.
+#define BUF_LEN     (1 << BUF_BITS)             // Buffer length.
+#define BUF_MASK    ((uint16_t)(BUF_LEN - 1))   // Buffer mask.
+float       ClaBuf[BUF_LEN];                    // Buffer to store filtered samples.
+float       AdcBuf[BUF_LEN];                    // Buffer for un-filtered samples.
+uint16_t    ClaBufIdx   = 0;                    // Buffer index for ClaBuf.
+uint16_t    AdcBufIdx   = 0;                    // Buffer index for AdcBufIdx.
+uint16_t    LedCtr      = 0;
+#pragma DATA_SECTION(filter_out,"Cla1ToCpuMsgRAM");
+float filter_out;
+#pragma DATA_SECTION(filter_in,"Cla1ToCpuMsgRAM");
+float filter_in;
+#define FIR_LEN     32
+const float coeffs[FIR_LEN] = {
+    -0.0006103139426196,-0.005592184655005,-0.01292485322836 ,-0.01926532895538,
+    -0.01762908502044  ,-0.004303218458627, 0.01563724674838 , 0.02815885544785,
+     0.01902562195175  ,-0.01290472511163 ,-0.04827630093442 ,-0.0545188692635,
+    -0.006464860729499 , 0.09270744275212 , 0.2064249836238  , 0.2822241125204,
+     0.2822241125204   , 0.2064249836238  , 0.09270744275212 ,-0.006464860729499,
+    -0.0545188692635   ,-0.04827630093442 ,-0.01290472511163 , 0.01902562195175,
+     0.02815885544785  , 0.01563724674838 ,-0.004303218458627,-0.01762908502044,
+    -0.01926532895538  ,-0.01292485322836 ,-0.005592184655005,-0.0006103139426196
+};
+float delay[FIR_LEN];
 
 //
 // Main
 //
 void main(void)
 {
-
+    // Configure system clock and PLL, enable peripherals, and configure
+    // flash if used.
+    Device_init();
+    // Initialize the PIE module and vector table.
+    Interrupt_initModule();
+    Interrupt_initVectorTable();
+	Board_init();
+    // Enable global interrupts.
+    EINT;
+    // Enable real-time debug.
+    ERTM;
+    for(;;) {
+        NOP;  // Do nothing.
+    }
 }
-
+// FIR LP filter task.
+__attribute__((interrupt)) void Cla1Task1(void)
+{
+    int16_t i;
+    uint16_t tmpData;
+    filter_out = 0.0;
+    // Uncomment this to debug the CLA while connected to the debugger
+    //__mdebugstop();
+    // Read in new sample and subtract off the bias.
+    tmpData = ADC_readResult(myADC0_RESULT_BASE, myADC0_SOC0);
+    filter_in = ((float)(tmpData) - (float)(1 << (ADC_BITS - 1)))*ADC_VSTEP;
+    // Shift the delay line to the right by 1.
+    #pragma UNROLL(FIR_LEN - 1)
+    for (i = FIR_LEN - 1; i > 0; i--) {
+        delay[i] = delay[i-1];
+        filter_out += coeffs[i]*delay[i];
+    }
+    // Add the new ADC sample to the delay line.
+    delay[0] = filter_in;
+    filter_out += coeffs[0]*delay[0];
+}
+__attribute__((interrupt)) void Cla1Task8(void)
+{
+    int16_t i;
+    for (i = 0; i < ((int16_t)FIR_LEN); i++) {
+        delay[i] = 0.0;
+    }
+}
+__interrupt void cla1Isr1(void)
+{
+    // Clear interrupt flags.
+    ADC_clearInterruptStatus(myADC0_BASE, ADC_INT_NUMBER1);
+    Interrupt_clearACKGroup(INT_myCLA01_INTERRUPT_ACK_GROUP);
+    // Store raw ADC sample in AdcBuf.
+    AdcBuf[AdcBufIdx++] = filter_in;
+    AdcBufIdx &= BUF_MASK;
+    // Store filtered output in ClaBuf.
+    ClaBuf[ClaBufIdx++] = filter_out;
+    ClaBufIdx &= BUF_MASK;
+    // Toggle LED1 at a rate of 1Hz.
+    if (LedCtr++ >= 8000) {
+        GPIO_togglePin(myBoardLED0_GPIO);
+        LedCtr = 0;
+    }
+}
 //
 // End of File
 //
